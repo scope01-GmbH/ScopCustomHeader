8cb8640ba5dca2741c2d1f8d21b70c8a13fa4fc1
Bogdan <bogdan.ciumac@scope01.com>
2020-03-06 14:04:24 +0100

    phpdoc added

--- ScopCustomHeader/src/ScopCustomHeader.php
+++ ScopCustomHeader/src/ScopCustomHeader.php
@@ -1,4 +1,5 @@
 <?php declare(strict_types=1);
+
 /**
  * Implemented by scope01 GmbH team https://scope01.com
  *
@@ -12,8 +13,15 @@ namespace Scop\ScopCustomHeader;
 use Shopware\Core\Framework\Plugin;
 use Shopware\Core\Framework\Plugin\Context\UninstallContext;
 
+/**
+ * Class ScopCustomHeader
+ * @package Scop\ScopCustomHeader
+ */
 class ScopCustomHeader extends Plugin
 {
+    /**
+     * @param UninstallContext $uninstallContext
+     */
     public function uninstall(UninstallContext $uninstallContext): void
     {
         parent::uninstall($uninstallContext);
